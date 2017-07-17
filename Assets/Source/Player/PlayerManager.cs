45795239fbf9b10cf528502539ffc675177a5116
LinYunqiang <linyq@gamebegin.com>
2017-07-17 16:41:20 +0800

    添加文件读取

---
+++ Assets/Source/Player/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+public class PlayerManager : PSingleton<PlayerManager> {
+	public const string CONFIG_PATH = "PlayerConfig";
+
+	protected override void Init()
+	{
+		CsvFile file =  FileUtility.LoadCsvFile(CONFIG_PATH);
+		if(file != null)
+		{
+			int head = file.HeaderCount;
+			int row = file.RecordCount;
+			for (int i = 0; i < row; i++)
+			{
+				CsvRecord record = file[i];
+				PlayerConfig config = new PlayerConfig(record);
+			}
+		}
+	}
+}
